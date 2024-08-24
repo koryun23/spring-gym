4007804fb37029643a41051e1cc73d06fa3c7500
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-24 23:25:07 +0400

    Implementing the entity classes of Trainer, Trainee and User

--- src/main/java/org/example/entity/Trainee.java
+++ src/main/java/org/example/entity/Trainee.java
@@ -1,4 +1,31 @@
 package org.example.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Trainee extends User {
+
+    private Date dateOfBirth;
+    private String address;
+    private Long userId;
+
+    public Trainee(String firstName, String lastName, String username, String password, boolean isActive, Date dateOfBirth, String address, Long userId) {
+        super(firstName, lastName, username, password, isActive);
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Trainee trainee = (Trainee) o;
+        return Objects.equals(dateOfBirth, trainee.dateOfBirth) && Objects.equals(address, trainee.address) && Objects.equals(userId, trainee.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dateOfBirth, address, userId);
+    }
 }
--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,4 +1,63 @@
 package org.example.entity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class User {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(User.class);
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean isActive;
+
+    public User(String firstName, String lastName, String username, String password, boolean isActive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.isActive = isActive;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
