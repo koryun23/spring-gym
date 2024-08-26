0b3eb7eae463305e23908ab225094de03373a3ab
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-27 00:32:23 +0400

    Adding the SpringBootApplication to the Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,30 @@
 package org.example;
 
 import org.example.entity.Trainee;
+import org.example.entity.Trainer;
 import org.example.helper.DateConverter;
 import org.example.repository.impl.TraineeStorageImpl;
+import org.example.repository.impl.TrainerStorageImpl;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(Main.class, args);
-        DateConverter dateConverter = (DateConverter) context.getBean("dateConverter");
         TraineeStorageImpl traineeStorage = context.getBean(TraineeStorageImpl.class);
-        traineeStorage.parseMemoryFile();
-        traineeStorage.add(new Trainee(
+        TrainerStorageImpl trainerStorage = context.getBean(TrainerStorageImpl.class);
+
+        trainerStorage.add(new Trainer(
                 1L,
                 "John",
-                "Sanchez",
-                "john3",
-                "pwd12",
+                "Terry",
+                "john26",
+                "pwd34",
                 true,
-                dateConverter.stringToDate("1950-10-19"),
-                "nyc"
+                "gym"
         ));
     }
 }
