2c69e37d06edde61e171e24bcd255ee35fe82cc4
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 21:00:50 +0400

    Adding logs for the TrainerServiceImpl class

--- src/main/java/org/example/service/impl/TrainerServiceImpl.java
+++ src/main/java/org/example/service/impl/TrainerServiceImpl.java
@@ -5,18 +5,23 @@ import org.example.entity.Trainer;
 import org.example.service.core.TrainerService;
 import org.example.service.params.TrainerCreateParams;
 import org.example.service.params.TrainerUpdateParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrainerServiceImpl implements TrainerService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrainerServiceImpl.class);
+
     @Autowired
     private TrainerDao trainerDao;
 
     @Override
     public Trainer create(TrainerCreateParams params) {
-        return trainerDao.save(new Trainer(
+        LOGGER.info("Creating a Trainer based on TrainerCreateParams - {}", params);
+        Trainer trainer = trainerDao.save(new Trainer(
                 params.getUserId(),
                 params.getFirstName(),
                 params.getLastName(),
@@ -25,11 +30,14 @@ public class TrainerServiceImpl implements TrainerService {
                 params.isActive(),
                 params.getSpecializationType()
         ));
+        LOGGER.info("Successfully created a Trainer based on TrainerCreateParams - {}, result - {}", params, trainer);
+        return trainer;
     }
 
     @Override
     public Trainer update(TrainerUpdateParams params) {
-        return trainerDao.update(new Trainer(
+        LOGGER.info("Updating a Trainer based on TrainerUpdateParams - {}", params);
+        Trainer trainer = trainerDao.update(new Trainer(
                 params.getUserId(),
                 params.getFirstName(),
                 params.getLastName(),
@@ -38,10 +46,15 @@ public class TrainerServiceImpl implements TrainerService {
                 params.isActive(),
                 params.getSpecializationType()
         ));
+        LOGGER.info("Successfully updated a Trainer based on TrainerUpdateParams - {}, result - {}", params, trainer);
+        return trainer;
     }
 
     @Override
     public Trainer select(Long trainerId) {
-        return trainerDao.get(trainerId);
+        LOGGER.info("Selecting a Trainer with an id of {}", trainerId);
+        Trainer trainer = trainerDao.get(trainerId);
+        LOGGER.info("Successfully selected a Trainer with an id of {}, result - {}", trainerId, trainer);
+        return trainer;
     }
 }
