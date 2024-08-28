1262c062f348090ed2a096d0d7cec6483e41f784
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 20:53:24 +0400

    Adding logs for the TrainingDao class

--- src/main/java/org/example/dao/impl/TrainingDao.java
+++ src/main/java/org/example/dao/impl/TrainingDao.java
@@ -3,32 +3,49 @@ package org.example.dao.impl;
 import org.example.dao.core.Dao;
 import org.example.entity.Training;
 import org.example.repository.impl.TrainingStorageImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrainingDao implements Dao<Training> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrainingDao.class);
+
     @Autowired
     private TrainingStorageImpl storage;
 
     @Override
     public Training get(Long id) {
-        return storage.get(id);
+        LOGGER.info("Retrieving a Training with an id of {}", id);
+        Training training = storage.get(id);
+        LOGGER.info("Successfully retrieved a Training with an id of {}, result - {}", id, training);
+        return training;
     }
 
     @Override
     public Training save(Training training) {
-        return storage.add(training);
+        LOGGER.info("Saving {}", training);
+        Training addedTraining = storage.add(training);
+        LOGGER.info("Successfully saved {}", addedTraining);
+        return addedTraining;
     }
 
     @Override
     public Training update(Training training) {
-        return storage.update(training);
+        LOGGER.info("Updating a Training with an id of {}", training.getTrainingId());
+        Training updatedTraining = storage.update(training);
+        LOGGER.info("Successfully updated a Training with an id of {}, result - {}", training.getTrainingId(), updatedTraining);
+        return updatedTraining;
     }
 
     @Override
     public boolean delete(Long id) {
-        return storage.remove(id);
+        LOGGER.info("Deleting a Training with an id of {}", id);
+        boolean success = storage.remove(id);
+        if(success) LOGGER.info("Successfully deleted a Training with an id of {}", id);
+        else LOGGER.error("Failed to delete a Training with an id of {}", id);
+        return success;
     }
 }
