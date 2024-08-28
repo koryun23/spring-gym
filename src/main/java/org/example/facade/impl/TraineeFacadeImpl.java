0d1d78ffb647ebe50326da21b38e27295578ea15
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 21:23:57 +0400

    Adding logs for the TraineeFacadeImpl class

--- src/main/java/org/example/facade/impl/TraineeFacadeImpl.java
+++ src/main/java/org/example/facade/impl/TraineeFacadeImpl.java
@@ -11,17 +11,23 @@ import org.example.facade.core.TraineeFacade;
 import org.example.service.core.TraineeService;
 import org.example.service.params.TraineeCreateParams;
 import org.example.service.params.TraineeUpdateParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TraineeFacadeImpl implements TraineeFacade {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TraineeFacadeImpl.class);
+
     @Autowired
     private TraineeService traineeService;
 
     @Override
     public TraineeCreationResponseDto createTrainee(TraineeCreationRequestDto requestDto) {
+
+        LOGGER.info("Creating a Trainee based on the TraineeCreationRequestDto - {}", requestDto);
         Trainee trainee = traineeService.create(new TraineeCreateParams(
                 requestDto.getUserId(),
                 requestDto.getFirstName(),
@@ -32,7 +38,7 @@ public class TraineeFacadeImpl implements TraineeFacade {
                 requestDto.getDateOfBirth(),
                 requestDto.getAddress()
         ));
-        return new TraineeCreationResponseDto(
+        TraineeCreationResponseDto responseDto = new TraineeCreationResponseDto(
                 trainee.getUserId(),
                 trainee.getFirstName(),
                 trainee.getLastName(),
@@ -42,10 +48,14 @@ public class TraineeFacadeImpl implements TraineeFacade {
                 trainee.getDateOfBirth(),
                 requestDto.getAddress()
         );
+        LOGGER.info("Successfully created a Trainee based on the TraineeCreationRequestDto - {}, response - {}", requestDto, responseDto);
+        return responseDto;
     }
 
     @Override
     public TraineeUpdateResponseDto updateTrainee(TraineeUpdateRequestDto requestDto) {
+        LOGGER.info("Updating a Trainee based on the TraineeUpdateRequestDto - {}", requestDto);
+
         Trainee trainee = traineeService.update(new TraineeUpdateParams(
                 requestDto.getUserId(),
                 requestDto.getFirstName(),
@@ -56,7 +66,7 @@ public class TraineeFacadeImpl implements TraineeFacade {
                 requestDto.getDateOfBirth(),
                 requestDto.getAddress()
         ));
-        return new TraineeUpdateResponseDto(
+        TraineeUpdateResponseDto responseDto = new TraineeUpdateResponseDto(
                 trainee.getUserId(),
                 trainee.getFirstName(),
                 trainee.getLastName(),
@@ -66,12 +76,15 @@ public class TraineeFacadeImpl implements TraineeFacade {
                 trainee.getDateOfBirth(),
                 trainee.getAddress()
         );
+        LOGGER.info("Successfully updated a Trainee based on the TraineeUpdateRequestDto - {}, response - {}", requestDto, responseDto);
+        return responseDto;
     }
 
     @Override
     public TraineeRetrievalResponseDto retrieveTrainee(Long id) {
+        LOGGER.info("Retrieving a Trainee with an id of {}", id);
         Trainee trainee = traineeService.select(id);
-        return new TraineeRetrievalResponseDto(
+        TraineeRetrievalResponseDto responseDto = new TraineeRetrievalResponseDto(
                 trainee.getUserId(),
                 trainee.getFirstName(),
                 trainee.getLastName(),
@@ -81,11 +94,16 @@ public class TraineeFacadeImpl implements TraineeFacade {
                 trainee.getDateOfBirth(),
                 trainee.getAddress()
         );
+        LOGGER.info("Successfully retrieved a Trainee with an id of {}, response - {}", id, responseDto);
+        return responseDto;
     }
 
     @Override
     public TraineeDeletionResponseDto deleteTrainee(Long id) {
+        LOGGER.info("Deleting a Trainee with an id of {}", id);
         boolean success = traineeService.delete(id);
-        return new TraineeDeletionResponseDto(success);
+        TraineeDeletionResponseDto responseDto = new TraineeDeletionResponseDto(success);
+        LOGGER.info("Successfully deleted a Trainee with an id of {}, response - {}", id, responseDto);
+        return responseDto;
     }
 }
