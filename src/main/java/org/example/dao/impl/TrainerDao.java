7d57fc4620683b3f3026a198ae7ab8b815b25f07
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 20:49:37 +0400

    Adding logs for the TrainerDao class

--- src/main/java/org/example/dao/impl/TrainerDao.java
+++ src/main/java/org/example/dao/impl/TrainerDao.java
@@ -3,32 +3,49 @@ package org.example.dao.impl;
 import org.example.dao.core.Dao;
 import org.example.entity.Trainer;
 import org.example.repository.impl.TrainerStorageImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrainerDao implements Dao<Trainer> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrainerDao.class);
+
     @Autowired
     private TrainerStorageImpl storage;
 
     @Override
     public Trainer get(Long id) {
-        return storage.get(id);
+        LOGGER.info("Retrieving a Trainer with an id of {}", id);
+        Trainer trainer = storage.get(id);
+        LOGGER.info("Successfully retrieved a Trainer with an id of {}, result - {}", id, trainer);
+        return trainer;
     }
 
     @Override
     public Trainer save(Trainer trainer) {
-        return storage.add(trainer);
+        LOGGER.info("Saving {}", trainer);
+        Trainer addedTrainer = storage.add(trainer);
+        LOGGER.info("Successfully added {}", addedTrainer);
+        return addedTrainer;
     }
 
     @Override
     public Trainer update(Trainer trainer) {
-        return storage.update(trainer);
+        LOGGER.info("Updating a Trainer with an id of {}", trainer.getUserId());
+        Trainer updatedTrainer = storage.update(trainer);
+        LOGGER.info("Successfully updated a Trainer with an id of {}, result - {}", trainer.getUserId(), updatedTrainer);
+        return updatedTrainer;
     }
 
     @Override
     public boolean delete(Long id) {
-        return storage.remove(id);
+        LOGGER.info("Deleting a Trainer with an id of {}", id);
+        boolean success = storage.remove(id);
+        if(success) LOGGER.info("Successfully deleted a Trainer with an id of {}", id);
+        else LOGGER.error("Failed to delete a Trainer with an id of {}", id);
+        return success;
     }
 }
