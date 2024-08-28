557d905307e2b46a0466189f2c4fc262d3d0e7c8
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 20:58:08 +0400

    Adding logs for the TraineeServiceImpl class

--- src/main/java/org/example/service/impl/TraineeServiceImpl.java
+++ src/main/java/org/example/service/impl/TraineeServiceImpl.java
@@ -5,18 +5,23 @@ import org.example.entity.Trainee;
 import org.example.service.core.TraineeService;
 import org.example.service.params.TraineeCreateParams;
 import org.example.service.params.TraineeUpdateParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TraineeServiceImpl implements TraineeService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TraineeServiceImpl.class);
+
     @Autowired
     private TraineeDao traineeDao;
 
     @Override
     public Trainee create(TraineeCreateParams params) {
-        return traineeDao.save(new Trainee(
+        LOGGER.info("Creating a Trainee based on TraineeCreateParams - {}", params);
+        Trainee trainee = traineeDao.save(new Trainee(
                 params.getUserId(),
                 params.getFirstName(),
                 params.getLastName(),
@@ -26,11 +31,14 @@ public class TraineeServiceImpl implements TraineeService {
                 params.getDateOfBirth(),
                 params.getAddress()
         ));
+        LOGGER.info("Successfully created a Trainee based on TraineeCreateParams - {}, result - {}", params, trainee);
+        return trainee;
     }
 
     @Override
     public Trainee update(TraineeUpdateParams params) {
-        return traineeDao.update(new Trainee(
+        LOGGER.info("Updating a Trainee based on TraineeUpdateParams - {}", params);
+        Trainee trainee = traineeDao.update(new Trainee(
                 params.getUserId(),
                 params.getFirstName(),
                 params.getLastName(),
@@ -40,15 +48,24 @@ public class TraineeServiceImpl implements TraineeService {
                 params.getDateOfBirth(),
                 params.getAddress()
         ));
+        LOGGER.info("Successfully updated a Trainee based on TraineeUpdateParams - {}, result - {}", params, trainee);
+        return trainee;
     }
 
     @Override
     public boolean delete(Long traineeId) {
-        return traineeDao.delete(traineeId);
+        LOGGER.info("Deleting a Trainee with an id of {}", traineeId);
+        boolean success = traineeDao.delete(traineeId);
+        if (success) LOGGER.info("Successfully deleted a trainee with an id of {}", traineeId);
+        else LOGGER.error("Failed to delete a trainee with an id of {}", traineeId);
+        return success;
     }
 
     @Override
     public Trainee select(Long traineeId) {
-        return traineeDao.get(traineeId);
+        LOGGER.info("Selecting a Trainee with an id of {}", traineeId);
+        Trainee trainee = traineeDao.get(traineeId);
+        LOGGER.info("Successfully selected a Trainee with an id of {}, result - {}", traineeId, trainee);
+        return trainee;
     }
 }
