3e329829b8819a58fab57c2d2c739eae3e3d0781
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 16:46:37 +0400

    implementing the TrainerFacade logic

--- src/main/java/org/example/facade/core/TrainerFacade.java
+++ src/main/java/org/example/facade/core/TrainerFacade.java
@@ -1,4 +1,16 @@
 package org.example.facade.core;
 
+import org.example.dto.request.TrainerCreationRequestDto;
+import org.example.dto.request.TrainerUpdateRequestDto;
+import org.example.dto.response.TrainerRetrievalResponseDto;
+import org.example.dto.response.TrainerUpdateResponseDto;
+import org.example.dto.response.TrainerCreationResponseDto;
+
 public interface TrainerFacade {
+
+    TrainerCreationResponseDto createTrainer(TrainerCreationRequestDto requestDto);
+
+    TrainerUpdateResponseDto updateTrainer(TrainerUpdateRequestDto requestDto);
+
+    TrainerRetrievalResponseDto retrieveTrainer(Long trainerId);
 }
--- src/main/java/org/example/facade/impl/TrainerFacadeImpl.java
+++ src/main/java/org/example/facade/impl/TrainerFacadeImpl.java
@@ -1,6 +1,78 @@
 package org.example.facade.impl;
 
+import org.example.dto.request.TrainerCreationRequestDto;
+import org.example.dto.request.TrainerUpdateRequestDto;
+import org.example.dto.response.TrainerCreationResponseDto;
+import org.example.dto.response.TrainerRetrievalResponseDto;
+import org.example.dto.response.TrainerUpdateResponseDto;
+import org.example.entity.Trainer;
 import org.example.facade.core.TrainerFacade;
+import org.example.service.core.TrainerService;
+import org.example.service.params.TrainerCreateParams;
+import org.example.service.params.TrainerUpdateParams;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TrainerFacadeImpl implements TrainerFacade {
+
+    @Autowired
+    private TrainerService trainerService;
+
+    @Override
+    public TrainerCreationResponseDto createTrainer(TrainerCreationRequestDto requestDto) {
+        Trainer trainer = trainerService.create(new TrainerCreateParams(
+                requestDto.getUserId(),
+                requestDto.getFirstName(),
+                requestDto.getLastName(),
+                requestDto.getUsername(),
+                requestDto.getPassword(),
+                requestDto.isActive(),
+                requestDto.getSpecializationType()
+        ));
+        return new TrainerCreationResponseDto(
+                trainer.getUserId(),
+                trainer.getFirstName(),
+                trainer.getLastName(),
+                trainer.getUsername(),
+                trainer.getPassword(),
+                trainer.isActive(),
+                trainer.getSpecialization()
+        );
+    }
+
+    @Override
+    public TrainerUpdateResponseDto updateTrainer(TrainerUpdateRequestDto requestDto) {
+        Trainer trainer = trainerService.update(new TrainerUpdateParams(
+                requestDto.getUserId(),
+                requestDto.getFirstName(),
+                requestDto.getLastName(),
+                requestDto.getUsername(),
+                requestDto.getPassword(),
+                requestDto.isActive(),
+                requestDto.getSpecializationType()
+        ));
+
+        return new TrainerUpdateResponseDto(
+                trainer.getUserId(),
+                trainer.getFirstName(),
+                trainer.getLastName(),
+                trainer.getUsername(),
+                trainer.getPassword(),
+                trainer.isActive(),
+                trainer.getSpecialization()
+        );
+    }
+
+    @Override
+    public TrainerRetrievalResponseDto retrieveTrainer(Long trainerId) {
+        Trainer trainer = trainerService.select(trainerId);
+        return new TrainerRetrievalResponseDto(
+                trainer.getUserId(),
+                trainer.getFirstName(),
+                trainer.getLastName(),
+                trainer.getUsername(),
+                trainer.getPassword(),
+                trainer.isActive(),
+                trainer.getSpecialization()
+        );
+    }
 }
