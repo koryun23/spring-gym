68e7407eb3057370d8793b1bfa674c37549ec69f
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 20:46:07 +0400

    Adding logs for the TraineeDao class

--- src/main/java/org/example/dao/impl/TraineeDao.java
+++ src/main/java/org/example/dao/impl/TraineeDao.java
@@ -3,32 +3,49 @@ package org.example.dao.impl;
 import org.example.dao.core.Dao;
 import org.example.entity.Trainee;
 import org.example.repository.impl.TraineeStorageImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TraineeDao implements Dao<Trainee> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TraineeDao.class);
+
     @Autowired
     private TraineeStorageImpl storage;
 
     @Override
     public Trainee get(Long id) {
-        return storage.get(id);
+        LOGGER.info("Retrieving a Trainee with an id of {}", id);
+        Trainee trainee = storage.get(id);
+        LOGGER.info("Successfully retrieved a Trainee with an id of {}, result - {}", id, trainee);
+        return trainee;
     }
 
     @Override
     public Trainee save(Trainee trainee) {
-        return storage.add(trainee);
+        LOGGER.info("Saving {}", trainee);
+        Trainee addedTrainee = storage.add(trainee);
+        LOGGER.info("Successfully saved {}", addedTrainee);
+        return addedTrainee;
     }
 
     @Override
     public Trainee update(Trainee trainee) {
-        return storage.update(trainee);
+        LOGGER.info("Updating a Trainee with an id of {}", trainee.getUserId());
+        Trainee updatedTrainee = storage.update(trainee);
+        LOGGER.info("Successfully updated a Trainee with an id of {}, reslut - {}", trainee.getUserId(), updatedTrainee);
+        return updatedTrainee;
     }
 
     @Override
     public boolean delete(Long id) {
-        return storage.remove(id);
+        LOGGER.info("Deleting a trainee with an id of {}", id);
+        boolean remove = storage.remove(id);
+        if(remove) LOGGER.info("Successfully removed a trainee with an id of {}", id);
+        else LOGGER.error("Failed to remove a trainee with an id of {}", id);
+        return remove;
     }
 }
