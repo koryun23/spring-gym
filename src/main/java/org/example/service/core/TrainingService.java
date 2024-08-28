0b110ed35f7b0f09cc16ff5416a8a531e65abf53
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 15:13:08 +0400

    Implementing the logic for TrainingService

--- src/main/java/org/example/service/core/TrainingService.java
+++ src/main/java/org/example/service/core/TrainingService.java
@@ -1,4 +1,11 @@
 package org.example.service.core;
 
+import org.example.entity.Training;
+import org.example.service.params.TrainingCreateParams;
+
 public interface TrainingService {
+
+    Training create(TrainingCreateParams params);
+
+    Training select(Long id);
 }
--- src/main/java/org/example/service/impl/TrainingServiceImpl.java
+++ src/main/java/org/example/service/impl/TrainingServiceImpl.java
@@ -1,6 +1,33 @@
 package org.example.service.impl;
 
+import org.example.dao.impl.TrainingDao;
+import org.example.entity.Training;
 import org.example.service.core.TrainingService;
+import org.example.service.params.TrainingCreateParams;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TrainingServiceImpl implements TrainingService {
+
+    @Autowired
+    private TrainingDao trainingDao;
+
+    @Override
+    public Training create(TrainingCreateParams params) {
+        return trainingDao.save(new Training(
+                params.getTrainingId(),
+                params.getTraineeId(),
+                params.getTrainerId(),
+                params.getName(),
+                params.getTrainingType(),
+                params.getTrainingDate(),
+                params.getDuration()
+        ));
+    }
+
+    @Override
+    public Training select(Long id) {
+        return trainingDao.get(id);
+    }
 }
