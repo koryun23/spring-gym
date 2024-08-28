0aa58862cf86885650539be18f7d90529146d917
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 15:03:48 +0400

    Implementing the logic for TrainerService

--- src/main/java/org/example/service/core/TrainerService.java
+++ src/main/java/org/example/service/core/TrainerService.java
@@ -1,4 +1,14 @@
 package org.example.service.core;
 
+import org.example.entity.Trainer;
+import org.example.service.params.TrainerCreateParams;
+import org.example.service.params.TrainerUpdateParams;
+
 public interface TrainerService {
+
+    Trainer create(TrainerCreateParams params);
+
+    Trainer update(TrainerUpdateParams params);
+
+    Trainer select(Long trainerId);
 }
--- src/main/java/org/example/service/impl/TrainerServiceImpl.java
+++ src/main/java/org/example/service/impl/TrainerServiceImpl.java
@@ -1,6 +1,47 @@
 package org.example.service.impl;
 
+import org.example.dao.impl.TrainerDao;
+import org.example.entity.Trainer;
 import org.example.service.core.TrainerService;
+import org.example.service.params.TrainerCreateParams;
+import org.example.service.params.TrainerUpdateParams;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TrainerServiceImpl implements TrainerService {
+
+    @Autowired
+    private TrainerDao trainerDao;
+
+    @Override
+    public Trainer create(TrainerCreateParams params) {
+        return trainerDao.save(new Trainer(
+                params.getUserId(),
+                params.getFirstName(),
+                params.getLastName(),
+                params.getUsername(),
+                params.getPassword(),
+                params.isActive(),
+                params.getSpecializationType()
+        ));
+    }
+
+    @Override
+    public Trainer update(TrainerUpdateParams params) {
+        return trainerDao.update(new Trainer(
+                params.getUserId(),
+                params.getFirstName(),
+                params.getLastName(),
+                params.getUsername(),
+                params.getPassword(),
+                params.isActive(),
+                params.getSpecializationType()
+        ));
+    }
+
+    @Override
+    public Trainer select(Long trainerId) {
+        return trainerDao.get(trainerId);
+    }
 }
