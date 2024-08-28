a645b2830f457b1edb85c1e6886a153a5e3573a3
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-28 21:03:07 +0400

    Adding logs for the TrainingServiceImpl class

--- src/main/java/org/example/service/impl/TrainingServiceImpl.java
+++ src/main/java/org/example/service/impl/TrainingServiceImpl.java
@@ -4,18 +4,23 @@ import org.example.dao.impl.TrainingDao;
 import org.example.entity.Training;
 import org.example.service.core.TrainingService;
 import org.example.service.params.TrainingCreateParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrainingServiceImpl implements TrainingService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrainerServiceImpl.class);
+
     @Autowired
     private TrainingDao trainingDao;
 
     @Override
     public Training create(TrainingCreateParams params) {
-        return trainingDao.save(new Training(
+        LOGGER.info("Creating a Training according to the TrainingCreateParams - {}", params);
+        Training training = trainingDao.save(new Training(
                 params.getTrainingId(),
                 params.getTraineeId(),
                 params.getTrainerId(),
@@ -24,10 +29,15 @@ public class TrainingServiceImpl implements TrainingService {
                 params.getTrainingDate(),
                 params.getDuration()
         ));
+        LOGGER.info("Successfully created a Training according to the Training Create Params - {}, result - {}", params, training);
+        return training;
     }
 
     @Override
     public Training select(Long id) {
-        return trainingDao.get(id);
+        LOGGER.info("Selecting a Training with an id of {}", id);
+        Training training = trainingDao.get(id);
+        LOGGER.info("Successfully selected a Training with an id of {}, result - {}", id, training);
+        return training;
     }
 }
