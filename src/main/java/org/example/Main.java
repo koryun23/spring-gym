8162c71107694a314f60d256a3078551bf1430b1
Koryun <koryun.grigoryan.5@gmail.com>
2024-08-30 00:13:06 +0400

    Removing the SpringBootApplication annotation from the Main class and using AnnotationConfigApplicationContext instead of Application context

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,19 @@
 package org.example;
 
-import org.example.entity.*;
+import org.example.config.Config;
 import org.example.helper.DateConverter;
-import org.example.repository.impl.TraineeStorageImpl;
-import org.example.repository.impl.TrainerStorageImpl;
-import org.example.repository.impl.TrainingStorageImpl;
 import org.example.service.core.TraineeService;
 import org.example.service.impl.TraineeServiceImpl;
 import org.example.service.params.TraineeCreateParams;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = SpringApplication.run(Main.class, args);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Config.class);
+        context.refresh();
+
         DateConverter dateConverter = (DateConverter) context.getBean("dateConverter");
 
         TraineeService traineeService = context.getBean(TraineeServiceImpl.class);
