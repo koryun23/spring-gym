59f3bbb077d58be131f773daf129b26c1a401016
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-01 00:11:26 +0400

    Removing userId, username and password fields from TraineeCreationRequestDto

--- src/main/java/org/example/dto/request/TraineeCreationRequestDto.java
+++ src/main/java/org/example/dto/request/TraineeCreationRequestDto.java
@@ -5,42 +5,25 @@ import java.util.Objects;
 
 public class TraineeCreationRequestDto {
 
-    private Long userId;
     private String firstName;
     private String lastName;
-    private String username;
-    private String password;
     private boolean isActive;
     private Date dateOfBirth;
     private String address;
 
-    public TraineeCreationRequestDto(Long userId,
-                      String firstName,
-                      String lastName,
-                      String username,
-                      String password,
-                      boolean isActive,
-                      Date dateOfBirth,
-                      String address) {
-        this.userId = userId;
+    public TraineeCreationRequestDto(String firstName,
+                                     String lastName,
+                                     boolean isActive,
+                                     Date dateOfBirth,
+                                     String address) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.username = username;
-        this.password = password;
         this.isActive = isActive;
         this.dateOfBirth = dateOfBirth;
         this.address = address;
     }
 
     public TraineeCreationRequestDto() {
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
     }
 
     public String getFirstName() {
@@ -57,22 +40,6 @@ public class TraineeCreationRequestDto {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public boolean isActive() {
@@ -104,22 +71,23 @@ public class TraineeCreationRequestDto {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TraineeCreationRequestDto that = (TraineeCreationRequestDto) o;
-        return isActive == that.isActive && Objects.equals(userId, that.userId) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(username, that.username) && Objects.equals(password, that.password) && Objects.equals(dateOfBirth, that.dateOfBirth) && Objects.equals(address, that.address);
+        return isActive == that.isActive &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(dateOfBirth, that.dateOfBirth) &&
+                Objects.equals(address, that.address);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId, firstName, lastName, username, password, isActive, dateOfBirth, address);
+        return Objects.hash(firstName, lastName, isActive, dateOfBirth, address);
     }
 
     @Override
     public String toString() {
         return "TraineeDto{" +
-                "userId=" + userId +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 ", isActive=" + isActive +
                 ", dateOfBirth=" + dateOfBirth +
                 ", address='" + address + '\'' +
