a42b8a822772f7b6c614585affec80839c85f2ba
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-01 00:53:42 +0400

    Interacting with facades in the Main class to test the functionality(it works)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,15 @@
 package org.example;
 
 import org.example.config.Config;
+import org.example.dto.request.TraineeCreationRequestDto;
+import org.example.dto.request.TrainerCreationRequestDto;
+import org.example.dto.request.TrainingCreationRequestDto;
+import org.example.entity.SpecializationType;
+import org.example.entity.Trainer;
+import org.example.entity.TrainingType;
+import org.example.facade.core.TraineeFacade;
+import org.example.facade.core.TrainerFacade;
+import org.example.facade.core.TrainingFacade;
 import org.example.helper.DateConverter;
 import org.example.service.core.TraineeService;
 import org.example.service.impl.TraineeServiceImpl;
@@ -15,18 +24,32 @@ public class Main {
         context.refresh();
 
         DateConverter dateConverter = (DateConverter) context.getBean("dateConverter");
+        TraineeFacade traineeFacade = (TraineeFacade) context.getBean("traineeFacade");
+        TrainerFacade trainerFacade = (TrainerFacade) context.getBean("trainerFacade");
+        TrainingFacade trainingFacade = (TrainingFacade) context.getBean("trainingFacade");
 
-        TraineeService traineeService = context.getBean(TraineeServiceImpl.class);
-
-        traineeService.create(new TraineeCreateParams(
-                1L,
-                "Jack",
+        traineeFacade.createTrainee(new TraineeCreationRequestDto(
+                "John",
                 "Murray",
-                "jack7",
-                "pwd123",
                 true,
                 dateConverter.stringToDate("2024-10-10"),
-                "london"
+                "manchester"
+        ));
+
+        trainerFacade.createTrainer(new TrainerCreationRequestDto(
+                "Jack",
+                "Henderson",
+                true,
+                SpecializationType.FITNESS
+        ));
+
+        trainingFacade.createTraining(new TrainingCreationRequestDto(
+                1L,
+                1L,
+                "Aeoribics Training",
+                TrainingType.AEROBIC,
+                dateConverter.stringToDate("2024-12-12"),
+                1_000_000L
         ));
     }
 }
