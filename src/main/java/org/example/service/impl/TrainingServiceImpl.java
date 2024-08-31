acf7d854384f2d8169d496722f280a1651912789
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-01 00:52:54 +0400

    Fixing a bug in TrainingServiceImpl class: Changing the logger's inspected class name from TrainerServiceImpl to TrainingServiceImpl

--- src/main/java/org/example/service/impl/TrainingServiceImpl.java
+++ src/main/java/org/example/service/impl/TrainingServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class TrainingServiceImpl implements TrainingService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TrainerServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrainingServiceImpl.class);
 
     @Autowired
     private TrainingDaoImpl trainingDao;
