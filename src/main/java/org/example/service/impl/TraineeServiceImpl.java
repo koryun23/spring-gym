7a9fa44ade0166b8728ae15a4633e608f4538471
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-01 23:50:33 +0400

    Adding validations to the TraineeServiceImpl class

--- src/main/java/org/example/service/impl/TraineeServiceImpl.java
+++ src/main/java/org/example/service/impl/TraineeServiceImpl.java
@@ -8,6 +8,7 @@ import org.example.service.params.TraineeUpdateParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public Trainee create(TraineeCreateParams params) {
+        Assert.notNull(params, "TraineeCreateParams must not be null");
         LOGGER.info("Creating a Trainee based on TraineeCreateParams - {}", params);
         Trainee trainee = traineeDao.save(new Trainee(
                 params.getUserId(),
@@ -37,6 +39,7 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public Trainee update(TraineeUpdateParams params) {
+        Assert.notNull(params, "TraineeCreateParams must not be null");
         LOGGER.info("Updating a Trainee based on TraineeUpdateParams - {}", params);
         Trainee trainee = traineeDao.update(new Trainee(
                 params.getUserId(),
@@ -54,6 +57,7 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public boolean delete(Long traineeId) {
+        Assert.notNull(traineeId, "Trainee id must not be null");
         LOGGER.info("Deleting a Trainee with an id of {}", traineeId);
         boolean success = traineeDao.delete(traineeId);
         if (success) LOGGER.info("Successfully deleted a trainee with an id of {}", traineeId);
@@ -63,6 +67,7 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public Trainee select(Long traineeId) {
+        Assert.notNull(traineeId, "Trainee id must not be null");
         LOGGER.info("Selecting a Trainee with an id of {}", traineeId);
         Trainee trainee = traineeDao.get(traineeId);
         LOGGER.info("Successfully selected a Trainee with an id of {}, result - {}", traineeId, trainee);
@@ -71,6 +76,8 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public Trainee selectByUsername(String username) {
+        Assert.notNull(username, "Trainee username must not be null");
+        Assert.hasText(username, "Trainee username must not be empty");
         LOGGER.info("Selecting a Trainee with a username of {}", username);
         Trainee trainee = traineeDao.getByUsername(username);
         LOGGER.info("Successfully selected a trainee with a username of {}, result - {}", username, trainee);
@@ -79,6 +86,7 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public Optional<Trainee> findById(Long id) {
+        Assert.notNull(id, "Trainee id must not be null");
         LOGGER.info("Retrieved an optional Trainee with an id of {}", id);
         Optional<Trainee> optionalTrainee = traineeDao.findById(id);
         LOGGER.info("Successfully retrieved an optional Trainee with an id of {}, result - {}", id, optionalTrainee);
@@ -87,6 +95,8 @@ public class TraineeServiceImpl implements TraineeService {
 
     @Override
     public Optional<Trainee> findByUsername(String username) {
+        Assert.notNull(username, "Trainee username must not be null");
+        Assert.hasText(username, "Trainee username must not be empty");
         LOGGER.info("Retrieved an optional Trainee with a username of {}", username);
         Optional<Trainee> optionalTrainee = traineeDao.findByUsername(username);
         LOGGER.info("Successfully retrieved an optional Trainee with an username of {}, result - {}", username, optionalTrainee);
