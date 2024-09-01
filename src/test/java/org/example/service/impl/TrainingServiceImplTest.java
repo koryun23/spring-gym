988fc8e019fbfcccb04ebea1e1707d5337c95d80
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-02 00:35:37 +0400

    Crating tests for testing the functionality of TrainingServiceImpl

---
+++ src/test/java/org/example/service/impl/TrainingServiceImplTest.java
@@ -0,0 +1,45 @@
+package org.example.service.impl;
+
+import org.assertj.core.api.Assertions;
+import org.example.dao.core.TrainingDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TrainingServiceImplTest {
+
+    private TrainingServiceImpl testSubject;
+
+    @Mock
+    private TrainingDao trainingDao;
+
+    @BeforeEach
+    public void init() {
+        testSubject = new TrainingServiceImpl();
+    }
+
+    @Test
+    public void testCreateWhenNull() {
+        Assertions.assertThatThrownBy(() -> testSubject.create(null))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void selectWhenNull() {
+        Assertions.assertThatThrownBy(() -> testSubject.select(null))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void findByIdWhenNull() {
+        Assertions.assertThatThrownBy(() -> testSubject.findById(null))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
