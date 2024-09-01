edb5fcc372749605511b48227534b34531a8fb97
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-02 00:30:54 +0400

    Adding validations to the TrainerServiceImpl class and creating tests for testing the functionality of the class

--- src/main/java/org/example/service/impl/TrainerServiceImpl.java
+++ src/main/java/org/example/service/impl/TrainerServiceImpl.java
@@ -8,6 +8,7 @@ import org.example.service.params.TrainerUpdateParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public Trainer create(TrainerCreateParams params) {
+        Assert.notNull(params, "TrainerCreateParams must not be null");
         LOGGER.info("Creating a Trainer based on TrainerCreateParams - {}", params);
         Trainer trainer = trainerDao.save(new Trainer(
                 params.getUserId(),
@@ -36,6 +38,7 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public Trainer update(TrainerUpdateParams params) {
+        Assert.notNull(params, "TrainerUpdateParams must not be null");
         LOGGER.info("Updating a Trainer based on TrainerUpdateParams - {}", params);
         Trainer trainer = trainerDao.update(new Trainer(
                 params.getUserId(),
@@ -52,6 +55,7 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public Trainer select(Long trainerId) {
+        Assert.notNull(trainerId, "Trainer id must not be null");
         LOGGER.info("Selecting a Trainer with an id of {}", trainerId);
         Trainer trainer = trainerDao.get(trainerId);
         LOGGER.info("Successfully selected a Trainer with an id of {}, result - {}", trainerId, trainer);
@@ -60,6 +64,8 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public Trainer selectByUsername(String username) {
+        Assert.notNull(username, "Trainer username must not be null");
+        Assert.hasText(username, "Trainer username must not be empty");
         LOGGER.info("Selecting a Trainer with a username of {}", username);
         Trainer trainer = trainerDao.getByUsername(username);
         LOGGER.info("Successfully selected a Trainer with a username of {}, result - {}", username, trainer);
@@ -68,6 +74,7 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public Optional<Trainer> findById(Long id) {
+        Assert.notNull(id, "Trainer id must not be null");
         LOGGER.info("Retrieving an optional Trainer with an id of {}", id);
         Optional<Trainer> optionalTrainer = trainerDao.findById(id);
         LOGGER.info("Successfully retrieved an optional Trainer with an id of {}, result - {}", id, optionalTrainer);
@@ -76,6 +83,8 @@ public class TrainerServiceImpl implements TrainerService {
 
     @Override
     public Optional<Trainer> findByUsername(String username) {
+        Assert.notNull(username, "Trainer username must not be null");
+        Assert.hasText(username, "Trainer username must not be empty");
         LOGGER.info("Retrieved an optional Trainer with a username of {}", username);
         Optional<Trainer> optionalTrainer = trainerDao.findByUsername(username);
         LOGGER.info("Successfully retrieved an optional Trainer with a username of {}, result - {}", username, optionalTrainer);
