311d19e8f30c7465d33de7fac4df7800bcd12827
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-01 22:26:34 +0400

    Adding validations to the training dao impl class and removing unnecessary methods from it

--- src/main/java/org/example/dao/core/TrainingDao.java
+++ src/main/java/org/example/dao/core/TrainingDao.java
@@ -6,9 +6,4 @@ import java.util.Optional;
 
 public interface TrainingDao extends Dao<Training> {
 
-    Training getByUsername(String username);
-
-    Optional<Training> findByUsername(String username);
-
-    Optional<Training> findById(Long id);
 }
--- src/main/java/org/example/dao/impl/TrainingDaoImpl.java
+++ src/main/java/org/example/dao/impl/TrainingDaoImpl.java
@@ -6,6 +6,7 @@ import org.example.repository.impl.TrainingStorageImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import java.util.Optional;
 
@@ -22,6 +23,7 @@ public class TrainingDaoImpl implements TrainingDao {
 
     @Override
     public Training get(Long id) {
+        Assert.notNull(id, "Training id must not be null");
         LOGGER.info("Retrieving a Training with an id of {}", id);
         Training training = storage.get(id);
         LOGGER.info("Successfully retrieved a Training with an id of {}, result - {}", id, training);
@@ -30,6 +32,7 @@ public class TrainingDaoImpl implements TrainingDao {
 
     @Override
     public Training save(Training training) {
+        Assert.notNull(training, "Training must not be null");
         LOGGER.info("Saving {}", training);
         Training addedTraining = storage.add(training);
         LOGGER.info("Successfully saved {}", addedTraining);
@@ -38,6 +41,7 @@ public class TrainingDaoImpl implements TrainingDao {
 
     @Override
     public Training update(Training training) {
+        Assert.notNull(training, "Training must not be null");
         LOGGER.info("Updating a Training with an id of {}", training.getTrainingId());
         Training updatedTraining = storage.update(training);
         LOGGER.info("Successfully updated a Training with an id of {}, result - {}", training.getTrainingId(), updatedTraining);
@@ -46,25 +50,11 @@ public class TrainingDaoImpl implements TrainingDao {
 
     @Override
     public boolean delete(Long id) {
+        Assert.notNull(id, "Training id must not be null");
         LOGGER.info("Deleting a Training with an id of {}", id);
         boolean success = storage.remove(id);
         if(success) LOGGER.info("Successfully deleted a Training with an id of {}", id);
         else LOGGER.error("Failed to delete a Training with an id of {}", id);
         return success;
-    }
-
-    @Override
-    public Training getByUsername(String username) {
-        return null;
-    }
-
-    @Override
-    public Optional<Training> findByUsername(String username) {
-        return Optional.empty();
-    }
-
-    @Override
-    public Optional<Training> findById(Long id) {
-        return Optional.empty();
     }
 }
