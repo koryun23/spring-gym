8c5e9e2ac94883ddd9d5d90af7c9da8d453b92c9
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-01 16:44:06 +0400

    Implementing the tests for TraineeStorageImpl class

--- src/test/java/org/example/repository/impl/TraineeStorageImplTest.java
+++ src/test/java/org/example/repository/impl/TraineeStorageImplTest.java
@@ -4,23 +4,31 @@ import org.assertj.core.api.Assertions;
 import org.example.entity.Trainee;
 import org.example.exception.TraineeNotFoundException;
 import org.example.helper.DateConverter;
+import org.example.repository.core.FileStorage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@ExtendWith(MockitoExtension.class)
 class TraineeStorageImplTest {
 
-    private Map<Long, Trainee> map;
-    private TraineeStorageImpl traineeStorage;
+    private TraineeStorageImpl testSubject;
+
+    @Mock
+    private FileStorage<Trainee> traineeFileStorage;
+
+    @Mock
+    private DateConverter dateConverter;
 
     @BeforeEach
     public void setUp() {
-        map = new HashMap<>();
+        Map<Long, Trainee> map = new HashMap<>();
         map.put(
                 1L, new Trainee(
                         1L,
@@ -33,12 +41,14 @@ class TraineeStorageImplTest {
                         "manchester"
                 )
         );
-        traineeStorage = new TraineeStorageImpl(map);
-        traineeStorage.setDateConverter(new DateConverter(new SimpleDateFormat("yyyy-MM-dd")));
+
+        testSubject = new TraineeStorageImpl(map);
+        testSubject.setTraineeFileStorage(traineeFileStorage);
     }
+
     @Test
     public void TestTraineeGetByIdWhenExists() {
-        Assertions.assertThat(traineeStorage.get(1L)).isEqualTo(new Trainee(
+        Assertions.assertThat(testSubject.get(1L)).isEqualTo(new Trainee(
                 1L,
                 "first",
                 "last",
@@ -52,7 +62,7 @@ class TraineeStorageImplTest {
 
     @Test
     public void TestTraineeGetByUsernameWhenExists() {
-        Assertions.assertThat(traineeStorage.getByUsername("username")).isEqualTo(new Trainee(
+        Assertions.assertThat(testSubject.getByUsername("username")).isEqualTo(new Trainee(
                 1L,
                 "first",
                 "last",
@@ -66,37 +76,58 @@ class TraineeStorageImplTest {
 
     @Test
     public void TestTraineeGetByIdWhenDoesNotExist() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.get(2L))
+        Assertions.assertThatThrownBy(() -> testSubject.get(2L))
                 .isExactlyInstanceOf(TraineeNotFoundException.class)
                 .hasMessageContaining("2");
     }
 
     @Test
     public void TestTraineeGetByUsernameWhenDoesNotExist() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.getByUsername("asdf"))
+        Assertions.assertThatThrownBy(() -> testSubject.getByUsername("asdf"))
                 .isExactlyInstanceOf(TraineeNotFoundException.class)
                 .hasMessageContaining("asdf");
     }
 
     @Test
     public void TestTraineeGetByIdWhenNull() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.get(null))
+        Assertions.assertThatThrownBy(() -> testSubject.get(null))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void TestTraineeGetByUsernameWhenNull() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.getByUsername(null))
+        Assertions.assertThatThrownBy(() -> testSubject.getByUsername(null))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void TestTraineeAdd() {
 
-        Assertions.assertThatThrownBy(() -> traineeStorage.get(2L))
+        HashMap<Long, Trainee> map = new HashMap<>();
+        map.put(1L, new Trainee(
+                1L,
+                "first",
+                "last",
+                "username",
+                "password",
+                true,
+                Date.valueOf("2024-10-10"),
+                "manchester"
+        ));
+        map.put(2L, new Trainee(
+                2L,
+                "first",
+                "last",
+                "username",
+                "password",
+                true,
+                Date.valueOf("2024-10-10"),
+                "manchester"
+        ));
+        Assertions.assertThatThrownBy(() -> testSubject.get(2L))
                 .isExactlyInstanceOf(TraineeNotFoundException.class);
 
-        traineeStorage.add(new Trainee(
+        testSubject.add(new Trainee(
                 2L,
                 "first",
                 "last",
@@ -107,7 +138,7 @@ class TraineeStorageImplTest {
                 "manchester"
         ));
 
-        Assertions.assertThat(traineeStorage.get(2L)).isEqualTo(new Trainee(
+        Assertions.assertThat(testSubject.get(2L)).isEqualTo(new Trainee(
                 2L,
                 "first",
                 "last",
@@ -121,14 +152,13 @@ class TraineeStorageImplTest {
 
     @Test
     public void TestTraineeAddWhenNull() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.add(null))
+        Assertions.assertThatThrownBy(() -> testSubject.add(null))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void TestTraineeRemove() {
-        Assertions.assertThat(map.size()).isEqualTo(1);
-        Assertions.assertThat(traineeStorage.get(1L)).isEqualTo(new Trainee(
+        Assertions.assertThat(testSubject.get(1L)).isEqualTo(new Trainee(
                         1L,
                         "first",
                         "last",
@@ -138,20 +168,31 @@ class TraineeStorageImplTest {
                         Date.valueOf("2024-10-10"),
                         "manchester"
         ));
-        traineeStorage.remove(1L);
-        Assertions.assertThat(map.size()).isZero();
+        testSubject.remove(1L);
+        Assertions.assertThatThrownBy(() -> testSubject.get(1L))
+                .isExactlyInstanceOf(TraineeNotFoundException.class);
     }
 
     @Test
     public void TestTraineeRemoveWhenNull() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.remove(null))
+        Assertions.assertThatThrownBy(() -> testSubject.remove(null))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void TestTraineeUpdate() {
-        Assertions.assertThat(map.size()).isOne();
-        Assertions.assertThat(traineeStorage.get(1L)).isEqualTo(new Trainee(
+        HashMap<Long, Trainee> map = new HashMap<>();
+        map.put(1L, new Trainee(
+                1L,
+                "first",
+                "last",
+                "username",
+                "password",
+                false,
+                Date.valueOf("2024-10-10"),
+                "manchester"
+        ));
+        Assertions.assertThat(testSubject.get(1L)).isEqualTo(new Trainee(
                 1L,
                 "first",
                 "last",
@@ -162,7 +203,7 @@ class TraineeStorageImplTest {
                 "manchester"
         ));
 
-        traineeStorage.update(new Trainee(
+        testSubject.update(new Trainee(
                 1L,
                 "first",
                 "last",
@@ -173,8 +214,7 @@ class TraineeStorageImplTest {
                 "manchester"
         ));
 
-        Assertions.assertThat(map.size()).isOne();
-        Assertions.assertThat(traineeStorage.get(1L)).isNotEqualTo(new Trainee(
+        Assertions.assertThat(testSubject.get(1L)).isNotEqualTo(new Trainee(
                 1L,
                 "first",
                 "last",
@@ -188,7 +228,7 @@ class TraineeStorageImplTest {
 
     @Test
     public void TestTraineeUpdateWhenNull() {
-        Assertions.assertThatThrownBy(() -> traineeStorage.update(null))
+        Assertions.assertThatThrownBy(() -> testSubject.update(null))
                 .isExactlyInstanceOf((IllegalArgumentException.class));
     }
 }
