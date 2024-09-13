bf59faf2c5539e1d75cf9b5f97d413a6ca9886c5
Koryun <koryun.grigoryan.5@gmail.com>
2024-09-13 16:04:35 +0400

    Removing redundant import statements from Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,6 @@
 package org.example;
 
-import java.sql.Date;
 import org.example.config.Config;
-import org.example.dto.request.TraineeCreationRequestDto;
-import org.example.dto.request.TraineeUpdateRequestDto;
-import org.example.dto.request.TrainerCreationRequestDto;
-import org.example.dto.request.TrainerUpdateRequestDto;
-import org.example.dto.request.TrainingCreationRequestDto;
-import org.example.entity.SpecializationType;
-import org.example.entity.TrainingType;
-import org.example.facade.core.TraineeFacade;
-import org.example.facade.core.TrainerFacade;
-import org.example.facade.core.TrainingFacade;
-import org.example.helper.DateConverter;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
