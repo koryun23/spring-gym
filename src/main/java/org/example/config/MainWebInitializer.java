404b9beef4e920821c8aa0d069e88d318214d06e
Koryun <koryun.grigoryan.5@gmail.com>
2024-10-07 17:59:18 +0400

    Adding mvc configs

---
+++ src/main/java/org/example/config/MainWebInitializer.java
@@ -0,0 +1,35 @@
+package org.example.config;
+
+import jakarta.servlet.FilterRegistration;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MainWebInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(WebConfig.class);
+        rootContext.setConfigLocation("org.example.config");
+
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        ServletRegistration.Dynamic dispatcher =
+            servletContext.addServlet("dispatcher",  new DispatcherServlet(rootContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+        FilterRegistration.Dynamic encodingFilter =
+            servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
