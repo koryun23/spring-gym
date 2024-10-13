ef362063f9bdeb3ef2ed5297bd822e7b2576280e
Koryun <koryun.grigoryan.5@gmail.com>
2024-10-14 01:03:33 +0400

    Deleting separate trainee mappers

--- src/main/java/org/example/mapper/trainee/TraineeUpdateRequestDtoToTraineeEntityMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.mapper.trainee;
-
-import org.example.dto.request.TraineeUpdateRequestDto;
-import org.example.entity.TraineeEntity;
-import org.example.mapper.Mapper;
-
-public interface TraineeUpdateRequestDtoToTraineeEntityMapper extends Mapper<TraineeUpdateRequestDto, TraineeEntity> {
-    TraineeEntity map(TraineeUpdateRequestDto requestDto);
-}
