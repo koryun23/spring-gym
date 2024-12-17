3d405e60b567c73d8a66c0de12b180258ecf9c46
Koryun <koryun.grigoryan.5@gmail.com>
2024-12-18 02:31:22 +0400

    Removing unnecessary code from JwtConverter

--- src/main/java/org/example/security/JwtConverter.java
+++ src/main/java/org/example/security/JwtConverter.java
@@ -1,8 +1,6 @@
 package org.example.security;
 
 import java.util.List;
-import org.example.entity.user.UserEntity;
-import org.example.service.core.user.UserService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,23 +11,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtConverter implements Converter<Jwt, JwtAuthenticationToken> {
 
-    private final UserService userService;
-
-    public JwtConverter(UserService userService) {
-        this.userService = userService;
-    }
-
     @SuppressWarnings("unchecked")
     @Override
     @NonNull
     public JwtAuthenticationToken convert(Jwt source) {
 
-        String username = (String) source.getClaim("username");
-        UserEntity user = userService.getByUsername(username);
-
         List<String> roles = source.getClaim("roles");
-        System.out.println(roles);
-        return new JwtAuthenticationToken(source,
-            roles.stream().map(SimpleGrantedAuthority::new).toList());
+        return new JwtAuthenticationToken(source, roles.stream().map(SimpleGrantedAuthority::new).toList());
     }
 }
