ec57bc848254a8b4cbb508764272db9818040a24
Koryun <koryun.grigoryan.5@gmail.com>
2025-02-05 02:30:40 +0400

    Adding logs to the strategy classes (add and delete)

--- trainer-working-hours-service/src/main/java/com/example/strategy/TrainerWorkingHoursAddStrategy.java
+++ trainer-working-hours-service/src/main/java/com/example/strategy/TrainerWorkingHoursAddStrategy.java
@@ -29,11 +29,19 @@ public class TrainerWorkingHoursAddStrategy implements TrainerWorkingHoursUpdate
 
         TrainerEntity updatedTrainerEntity = null;
         if (optionalTrainerEntity.isPresent()) {
+            log.info("Trainer's working hours are already registered in the month {} of year {}",
+                trainerEntity.getTrainingMonth(), trainerEntity.getTrainingYear());
             TrainerEntity persistedTrainerEntity = optionalTrainerEntity.get();
             persistedTrainerEntity.setDuration(persistedTrainerEntity.getDuration() + trainerEntity.getDuration());
+            log.info("Persisting trainer entity - {}", persistedTrainerEntity);
             updatedTrainerEntity = trainerRepository.save(persistedTrainerEntity);
+            log.info("Successfully persisted trainer entity - {}, result - {}", persistedTrainerEntity,
+                updatedTrainerEntity);
         } else {
+            log.info("Registering trainer's working hours in the month {} of year {}", trainerEntity.getTrainingMonth(),
+                trainerEntity.getTrainingYear());
             updatedTrainerEntity = trainerRepository.save(trainerEntity);
+            log.info("Successfully registered trainer's working hours in the month {} of year {}, result - {}", trainerEntity.getTrainingMonth(), trainerEntity.getTrainingYear(), updatedTrainerEntity);
         }
 
         log.info("Successfully added to the working hours of {}, new value - {}", trainerEntity.getTrainerUsername(),
--- trainer-working-hours-service/src/main/java/com/example/strategy/TrainerWorkingHoursRemoveStrategy.java
+++ trainer-working-hours-service/src/main/java/com/example/strategy/TrainerWorkingHoursRemoveStrategy.java
@@ -30,11 +30,19 @@ public class TrainerWorkingHoursRemoveStrategy implements TrainerWorkingHoursUpd
 
         TrainerEntity updatedTrainerEntity = null;
         if (optionalTrainerEntity.isPresent()) {
+            log.info("Trainer's working hours are already registered in the month {} of year {}",
+                trainerEntity.getTrainingMonth(), trainerEntity.getTrainingYear());
             TrainerEntity persistedTrainerEntity = optionalTrainerEntity.get();
             persistedTrainerEntity.setDuration(persistedTrainerEntity.getDuration() - trainerEntity.getDuration());
+            log.info("Persisting trainer entity - {}", persistedTrainerEntity);
             updatedTrainerEntity = trainerRepository.save(persistedTrainerEntity);
+            log.info("Successfully persisted trainer entity - {}, result - {}", persistedTrainerEntity,
+                updatedTrainerEntity);
         } else {
+            log.info("Registering trainer's working hours in the month {} of year {}", trainerEntity.getTrainingMonth(),
+                trainerEntity.getTrainingYear());
             updatedTrainerEntity = trainerRepository.save(trainerEntity);
+            log.info("Successfully registered trainer's working hours in the month {} of year {}, result - {}", trainerEntity.getTrainingMonth(), trainerEntity.getTrainingYear(), updatedTrainerEntity);
         }
 
         log.info("Successfully added to the working hours of {}, new value - {}", trainerEntity.getTrainerUsername(),
